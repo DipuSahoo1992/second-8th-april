e09726c8783af1555346eb87ab52d00e2bdc08cd
phalguni <phalgunisen7@gmail.com>
2024-04-09 08:49:38 +0530

    hrml added

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+book = {"name": "herry", "published at": 1775, "author": "dipoo sahoo"}
+
+
 
 def hello(request):
-    return HttpResponse("hello form my django + yoyo")
-
-
-# Create your views here.
+    return render(request, "index.html", {"mylist":book})
